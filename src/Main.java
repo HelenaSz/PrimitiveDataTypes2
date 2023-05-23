b6be9bb9673c58dae5735cd9a0e506c17d97430b
Neto work <lenas_2003@mail.ru>
2023-05-23 11:51:29 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int account = 100;
+        int refill = 2000;
+        int percent = 1;
+
+        if (refill < 1000) {
+            System.out.println("Бонусы не начислены: " + (refill + account));
+        }
+
+        if (refill >= 1000) {
+            System.out.println("Бонусы начислены в размере 1 процента за каждые 100 руб: "
+                    + (account + refill + (refill * percent / 100)));
+        }
     }
 }
+
+
+
